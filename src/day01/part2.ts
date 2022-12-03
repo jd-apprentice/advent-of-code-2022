35d9f2c6d605d4cd97804823cb2970f829f33dcf
Jonathan Dyallo <emeraldusk@gmail.com>
2022-12-03 01:21:41 -0300

    day 1

--- src/day01/part2.ts
+++ src/day01/part2.ts
@@ -1,5 +1,11 @@
 // Advent of Code - Day 1 - Part Two
 
-export function part2(input: string): number {
-  return 0;
-}
+export const part2 = (input: string): number => {
+  return input
+    .split("\n\n")
+    .map((v) => v.split("\n").map(Number))
+    .map((a) => a.reduce((acc, next) => acc + next, 0))
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((acc, next) => acc + next, 0);
+};
